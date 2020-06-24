764880d0017232ca2a991b72214df68fdd5f743b
tomascavataio <tomascavataio@gmail.com>
2020-06-24 18:50:49 +0200

    Validations, and new design

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let numbers = document.getElementById("buttons")
+let numbers = document.getElementById("buttons");
 numbers.addEventListener("click", display);
 
 let screen = document.getElementById("screen");
@@ -6,39 +6,56 @@ let value = document.getElementById("selectedNumber");
 
 let log = document.getElementById("logUl");
 let previousOperations = document.getElementById("history");
+let clearing = document.getElementById("clearing").addEventListener("click", clear)
+
+let validation = numbers.addEventListener("click", validate);
+
+let history = [];
+let lastOperation;
 
 function display(number) {
-    let type = number.target.className
+    validate(number);
+    let type = number.target.className;
     let first = number.target.innerText;
     first == "C" ? clear() : null;
     type == "number" || type == "operator" ? value.innerText = value.innerText + first : null;
 }
 
 function calculate() {
-    history.push(`${value.innerText} = ${eval(value.innerText)}`)
-    value.innerText = eval(value.innerText)
-    debugger
+    history.push(`${value.innerText} = ${eval(value.innerText)}`);
+    value.innerText = eval(value.innerText);
     const li = document.createElement("li");
     log.appendChild(li);
     lastOperation = history[history.length - 1];
     li.innerText = lastOperation;
 }
 
-function clear() {
+function clean() {
     value.innerText = "";
 }
 
-let history = [];
-let lastOperation;
+function clear() {
+    log.innerText = "";
+    history = [];
+}
 
 function showHistory() {
-    previousOperations.style.opacity = "1"
+    previousOperations.style.opacity = "1";
 }
 
 function reveal() {
-    previousOperations.style.opacity = 0
+    previousOperations.style.opacity = 0;
 }
 
 function back() {
     value.innerText = value.innerText.substring(0, value.innerText.length - 1);
+}
+
+function validate(button) {
+    let lastCharacter = value.innerText[value.innerText.length - 1];
+    if (lastCharacter == "/" || lastCharacter == "*" || lastCharacter == "+" || lastCharacter == "-" || lastCharacter == "%" || lastCharacter == ".") {
+        if (button.target.className == "operator") {
+            calculator.preventDefault();
+        }
+    }
 }
